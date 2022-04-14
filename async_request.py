bb35074c2c54a24edae9cb2304347c5e5182b001
Adrian Garza <95837948+TheDragmaster@users.noreply.github.com>
2022-04-14 17:02:30 -0500

    Add files via upload

---
+++ async_request.py
@@ -0,0 +1,30 @@
+import asyncio
+import aiohttp
+import os
+import time
+
+api_key = os.getenv('ALPHAVANTAGE_API_KEY')
+url = 'https://www.alphavantage.co/query?function=OVERVIEW&symbol={}&apikey={}'
+symbols = ['AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT','AAPL', 'GOOG', 'TSLA', 'MSFT',]
+results = []
+
+start = time.time()
+
+def get_tasks(session):
+    tasks = []
+    for symbol in symbols:
+        tasks.append(session.get(url.format(symbol, api_key), ssl=False))
+        return tasks
+async def get_symbols():
+    async with aiohttp.ClientSession() as session:
+        tasks = get_tasks(session)
+        responses = await asyncio.gather(*tasks)
+        for response in responses:
+            results.append(await response.json())
+
+
+asyncio.run(get_symbols())
+end = time.time()
+total_time = end - start
+print("It took {} seconds to make {} API calls".format(total_time, len(symbols)))
+print('You did it!')
